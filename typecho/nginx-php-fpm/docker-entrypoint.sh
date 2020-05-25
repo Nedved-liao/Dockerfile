8eeabf2aee52656f6c0505184a8484c4ec1cb0cb
Nedved-liao <liao@nedved.st>
2020-05-25 14:14:59 +0800

    add the opcache

--- typecho/nginx-php-fpm/docker-entrypoint.sh
+++ typecho/nginx-php-fpm/docker-entrypoint.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 user=nginx
 group=nginx
+if [ ! -e index.php ] && [ ! -e admin/welcome.php ]; then
+	# if the directory exists and typecho doesn't appear to be installed
+	if [ "$(id -u)" = '0' ] && [ "$(stat -c '%u:%g' .)" = '0:0' ]; then
+		chown "$user:$group" .
+	fi
 
-	if [ ! -e index.php ] && [ ! -e admin/welcome.php ]; then
-		# if the directory exists and typecho doesn't appear to be installed
-		if [ "$(id -u)" = '0' ] && [ "$(stat -c '%u:%g' .)" = '0:0' ]; then
-			chown "$user:$group" .
-		fi
-
-		echo >&2 "typecho not found in $PWD - copying now..."
-		if [ -n "$(ls -A)" ]; then
-			echo >&2 "WARNING: $PWD is not empty! (copying anyhow)"
-		fi
-		sourceTarArgs=(
-			--create
-			--file -
-			--directory /tmp/build
-			--owner "$user" --group "$group"
-		)
-		targetTarArgs=(
-			--extract
-			--file -
-		)
-		if [ "$user" != '0' ]; then
-			# avoid "tar: .: Cannot utime: Operation not permitted" and "tar: .: Cannot change mode to rwxr-xr-x: Operation not permitted"
-			targetTarArgs+=( --no-overwrite-dir )
-		fi
-        tar "${sourceTarArgs[@]}" . | tar "${targetTarArgs[@]}"
-        echo >&2 "Complete! typecho has been successfully copied to $PWD"
-    fi
+	echo >&2 "typecho not found in $PWD - copying now..."
+	if [ -n "$(ls -A)" ]; then
+		echo >&2 "WARNING: $PWD is not empty! (copying anyhow)"
+	fi
+	sourceTarArgs=(
+		--create
+		--file -
+		--directory /tmp/build
+		--owner "$user" --group "$group"
+	)
+	targetTarArgs=(
+		--extract
+		--file -
+	)
+	if [ "$user" != '0' ]; then
+		# avoid "tar: .: Cannot utime: Operation not permitted" and "tar: .: Cannot change mode to rwxr-xr-x: Operation not permitted"
+		targetTarArgs+=( --no-overwrite-dir )
+	fi
+    tar "${sourceTarArgs[@]}" . | tar "${targetTarArgs[@]}"
+    echo >&2 "Complete! typecho has been successfully copied to $PWD"
+fi
 
 # start nginx and php-fpm
 supervisord -c /etc/supervisord.conf
